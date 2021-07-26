c4a2aaa0eb6470227c2ea3689dcd64d589dfed68
codewc <1164004116@qq.com>
2021-07-26 16:49:20 +0800

    Remove parm of private web sockets endpoint acceptUserMessage (add setup version)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='kucoin-python',
-    version='v1.0.7',
+    version='v1.0.8',
     packages=['kucoin', 'kucoin/base_request', 'kucoin/margin', 'kucoin/market', 'kucoin/trade', 'kucoin/user',
               'kucoin/websocket', 'kucoin/ws_token'],
     license="MIT",
