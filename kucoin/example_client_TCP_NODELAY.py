72e6c5bb02bf271e2a4250c60657423d3bf156cf
allenye1024 <131139521+allenye1024@users.noreply.github.com>
2024-02-19 13:09:00 +0800

    Update example_client_TCP_NODELAY.py

--- kucoin/example_client_TCP_NODELAY.py
+++ kucoin/example_client_TCP_NODELAY.py
@@ -5,7 +5,7 @@ from kucoin.client import Trade
 import time
 
 
-client = Trade(key='5c2db93503aa674c74a31734', secret='f03a5284-5c39-4aaa-9b20-dea10bdcf8e3', passphrase='QWIxMjM0NTY3OCkoKiZeJSQjQA==',
+client = Trade(key='', secret='', passphrase='',
                url='https://openapi-v2.kucoin.com')
 #client.TCP_NODELAY=1
 res=client.create_oco_order('FRM-USDT','buy','0.05','0.09','25','0.08','',"mark")
